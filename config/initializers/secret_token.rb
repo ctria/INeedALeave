cf8cf9326b9dda8a46bd1c3fad6d83f8193955a4
Christos Triantafyllidis <ctrianta@redhat.com>
2013-05-09 22:24:08 +0200

    Initial Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+INeedALeave::Application.config.secret_key_base = 'ba7ba97123e67d95ff3c47d32203ea45fe9ac1e48f19d920339d6fea9817a98318e0b62281692803adb108a66ac463d0bd283369ef21a30bd38f6975cf113d01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+INeedALeave::Application.config.session_store :cookie_store, key: '_INeedALeave_session'
